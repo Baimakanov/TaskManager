826d1a1a014ed2bd72299f56e20db8c1c714be7f
miras <baimakanovme@gmail.com>
2022-08-01 11:50:19 +0600

    tweaked todo

--- todo/views.py
+++ todo/views.py
@@ -59,12 +59,12 @@ def createtodo(request):
 
 @login_required
 def currenttodos(request):
-    todos = Todo.objects.filter(user = request.user, datecompleted__isnull=True)
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'todo/currenttodos.html', {'todos':todos})
 
 @login_required
 def completedtodos(request):
-    todos = Todo.objects.filter(user = request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
     return render(request, 'todo/completedtodos.html', {'todos':todos})
 
 @login_required
